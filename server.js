149060d9bee509d3053480a3ea52be7fe7d06794
NehalRadha <35211318+NehalRadha@users.noreply.github.com>
2018-02-17 16:29:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,11 +5,60 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne= {
+    title: 'Article One | Nehal',
+    heading:'Article One',
+    date: 'Feb 17,2018',
+    content: `<p>
+                    This is my content for First article.This is my content for First article.This is my content for First article.This is my content for First article.This is my content for First article.This is my content for First article.This is my content for First article.This is my content for First article.This is my content for First article.This is my content for First article.This is my content for First article.This is my content for First article.
+                </p>
+                <p>
+                    This is my content for First article.This is my content for First article.This is my content for First article.This is my content for First article.This is my content for First article.This is my content for First article.This is my content for First article.This is my content for First article.This is my content for First article.This is my content for First article.This is my content for First article.This is my content for First article.
+                </p>
+                <p>
+                    This is my content for First article.This is my content for First article.This is my content for First article.This is my content for First article.This is my content for First article.This is my content for First article.This is my content for First article.This is my content for First article.This is my content for First article.This is my content for First article.This is my content for First article.This is my content for First article.
+                </p>`
+};
+function createTemplate(data) {
+    var title=data.title;
+    var heading=data.title;
+    var date=data.title;
+    var content=data.content;
+    var htmlTemplate=`<html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container">
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr/>
+                <h3>${heading}</h3>
+                <div>
+                   ${date}
+                    
+                </div>
+                <div>
+                   ${content}
+                </div>
+            </div>
+            
+        </body>
+    </html>
+    `;
+    return htmlTemplate;
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res){
- res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));   
+    res.send(createTemplate(articleOne));   
 });
 app.get('/article-two', function (req, res){
     res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
