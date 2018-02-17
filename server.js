2d81b33b8d12bf0cdd18231dcfa8cb98326c1d7a
NehalRadha <35211318+NehalRadha@users.noreply.github.com>
2018-02-17 17:44:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -40,11 +40,13 @@ var articles={
     }
 };
 function createTemplate(data) {
-    var title=data.title;
-    var heading=data.heading;
-    var date=data.date;
-    var content=data.content;
-    var htmlTemplate=`<html>
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    
+    var htmlTemplate = `
+    <html>
         <head>
             <title>
                 ${title}
@@ -79,8 +81,8 @@ app.get('/', function (req, res) {
 });
 app.get('/:articleName', function (req, res){
     //articleName== article-one
-    //articleName=={}content object for article one..
-   var articleName=req.param.articleName;
+    //articleName=={}content object for article one
+   var articleName=req.params.articleName;
     res.send(createTemplate(articles[articleName]));   
 });
 
