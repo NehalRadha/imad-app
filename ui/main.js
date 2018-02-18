7e03147ae836aed89eaf60e1ff8aeac7c446ee31
NehalRadha <35211318+NehalRadha@users.noreply.github.com>
2018-02-18 19:00:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -13,15 +13,20 @@ button.onclick = function() {
     if(request.readyState=== XMLHttpRequest.DONE) {
        //Take some action
         if(request.status === 200){
-            var counter = request.responseText;
-            var span=document.getElementById("count");
-            span.innerHTML=counter.toString();
+            var names=request.responseText;
+            name=JSON.parse(names);
+            var list='';
+            for(var i=0; i<names.length; i++){
+                list+="<li>" + names[i] + "</li>";
+        }
+var ul = document.getElementById("namelist");
+ul.innerHTML=list;
         }
     }  
-   
+   //not yet done
 };
 // Make a request
-    request.open('GET', 'http://nehalradha95.imad.hasura-app.io/counter',true);
+    request.open('GET', 'http://nehalradha95.imad.hasura-app.io/submit-name?name=' + name,true);
     request.send(null);
 };
 
@@ -32,11 +37,5 @@ var submit=document.getElementById("submit_btn");
 submit.onclick= function() {
 //Make the request to the server and send the name
 //Capture a list of name and render it as a list
-var names=["name1", "name2", "name3", "name4"];
-var list='';
-for(var i=0; i<names.length; i++){
-    list+="<li>" + names[i] + "</li>";
-}
-var ul = document.getElementById("namelist");
-ul.innerHTML=list;
+
 };
