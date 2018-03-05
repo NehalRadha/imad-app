a39c16a80da76c2404bea6a48299d46fef273d96
NehalRadha <35211318+NehalRadha@users.noreply.github.com>
2018-03-05 14:13:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var config= {
 
 var app = express();
 app.use(morgan('combined'));
-app.use(bodyPareser.json());
+app.use(bodyParser.json());
 
 function createTemplate(data) {
     var title = data.title;
